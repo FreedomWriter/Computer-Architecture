8187e3125c195d5a395e05b04d904187d4d8171f
FreedomWriter <natalienetworks@gmail.com>
2020-10-11 20:00:23 -0500

    an attempt

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,27 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.LDI = LDI
+        self.PRN = PRN
+        self.HLT = HLT
+
+    def ram_read(self, pc):
+        return self.ram[pc]
+
+    def ram_write(self, pc, value):
+        self.ram[pc] = value
 
     def load(self):
         """Load a program into memory."""
@@ -40,26 +55,54 @@ class CPU:
         else:
             raise Exception("Unsupported ALU operation")
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+    # def trace(self):
+    #     """
+    #     Handy function to print out the CPU state. You might want to call this
+    #     from run() if you need help debugging.
+    #     """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+    #     print(f"TRACE: %02X | %02X %02X %02X |" % (
+    #         self.pc,
+    #         #self.fl,
+    #         #self.ie,
+    #         self.ram_read(self.pc),
+    #         self.ram_read(self.pc + 1),
+    #         self.ram_read(self.pc + 2)
+    #     ), end='')
 
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+    #     for i in range(8):
+    #         print(" %02X" % self.reg[i], end='')
 
-        print()
+    #     print()
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+
+        while running == True:
+            ir = self.ram[self.pc]
+
+            if ir == self.LDI:
+                self.ldi()
+
+            elif ir == self.PRN:
+                self.prn()
+
+            elif ir == self.HLT:
+                running = self.hlt(running)
+
+    def ldi(self):
+        address = self.ram[self.pc+1]
+        value = self.ram[self.pc+2]
+        self.ram_write(address, value)
+        self.pc += 3
+
+    def prn(self):
+        address = self.ram[self.pc+1]
+        value = self.ram_read(address)
+        print(value)
+        self.pc += 2
+
+    def hlt(self, running):
+        self.pc += 1
+        return False
